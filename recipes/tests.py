018ec6c1342588dd1bcafdf47826a8757c79e039
LorranFont <lorran0202fps@gmail.com>
2023-08-18 14:43:25 -0300

    corrigindo sintaxe

--- recipes/tests.py
+++ recipes/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        print('Iniciando os teste')
-        assert 1 != 3, 'informação errada'
+        print('teste...')
+        assert 1 != 3, 'informação certa'
