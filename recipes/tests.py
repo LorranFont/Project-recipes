128b41294adfdb46432b5c9aeba71204fa348b75
LorranFont <lorran0202fps@gmail.com>
2023-08-18 14:39:57 -0300

    dando inicio aos testes no django com pytest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        print('Iniciando os teste')
+        assert 1 != 3, 'informação errada'
