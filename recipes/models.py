5395f046bd8744a2a56f3170b8801f0ebaf7a2ed
LorranFont <lorran0202fps@gmail.com>
2023-10-17 15:05:18 -0300

    corringo espaçamento e linhas longas

--- recipes/models.py
+++ recipes/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self):
-        return self.name 
+        return self.name
 
 
 class Recipe(models.Model):
@@ -29,15 +29,15 @@ class Recipe(models.Model):
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
-    servings= models.IntegerField()
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_staps = models.TextField()
     preparation_staps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='') # noqa:  E501
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None) # noqa:  E501
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
