aaa35bd664f05775ff6c14f75146f33969b53d8e
LorranFont <lorran0202fps@gmail.com>
2023-10-24 23:16:14 -0300

    Testando se category e detail carregam a receita correta

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -12,6 +12,17 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))  # noqa:  E501
         self.assertAlmostEquals(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'this is a  category test'
+        # need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        # check if one recipe exists
+        self.assertIn(needed_title, content)
+
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1000}))  # noqa:  E501
         self.assertAlmostEquals(response.status_code, 404)
@@ -47,3 +58,22 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        needed_title = 'this is a  detail page - it load one recipe'
+
+        # need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(
+            reverse(
+                'recipes:recipe',
+                kwargs={
+                    'id': 1
+                }
+            )
+        )
+        content = response.content.decode('utf-8')
+
+        # check if one recipe exists
+        self.assertIn(needed_title, content)
